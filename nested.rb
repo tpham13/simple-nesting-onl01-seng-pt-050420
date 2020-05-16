9ad832e6dbcf349738c881d415b31f428bc49d06
Thanh Pham <tpham0123@gmail.com>
2020-05-16 22:13:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,8 +16,11 @@ def hopper
         }
      }
 
+programmer_hash[:grace_hopper]
 
 end
+ 
+
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -37,6 +40,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +59,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+programmer_hash[:dennis_ritchie][:languages][]
 end
 
 def adding_matz

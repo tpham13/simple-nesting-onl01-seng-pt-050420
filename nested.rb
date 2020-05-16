dfa5469480dc09522fa9f9836b14f76087c2fb78
Thanh Pham <tpham0123@gmail.com>
2020-05-16 22:24:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -70,7 +70,10 @@ def adding_matz
 # }
 
 	programmer_hash = 
- 		{
+ 		{:yukihiro_matsumoto => {
+ 		  :known_for => "Ruby",
+ 		  :languages => ["LISP", "C"]
+ 		},
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -84,8 +87,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-    newly_added_hash = {:yukihiro_matsumoto => {:known_for => "Ruby",:languages => ["LISP", "C"]}} 
-    programmer_hash << newly_added_hash
+  
 end
 
 def changing_alan

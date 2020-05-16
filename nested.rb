a6e47013ad27a2d00ed04a9f36edf98fd444b029
Thanh Pham <tpham0123@gmail.com>
2020-05-16 22:15:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,7 +59,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages]
+programmer_hash[:dennis_ritchie][:languages] = "C"
 end
 
 def adding_matz
